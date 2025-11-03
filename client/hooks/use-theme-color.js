f8426762d1f699d5964435a0ad82474766140729
ikeshav26 <keshavgilhotra4@gmail.com>
2025-11-03 23:16:22 +0530

    TSX converted to JSX

--- client/hooks/use-theme-color.js
+++ client/hooks/use-theme-color.js
@@ -3,13 +3,10 @@
  * https://docs.expo.dev/guides/color-schemes/
  */
 
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import { Colors } from '../constants/theme';
+import { useColorScheme } from './use-color-scheme';
 
-export function useThemeColor(
-  props: { light?: string; dark?: string },
-  colorName: keyof typeof Colors.light & keyof typeof Colors.dark
-) {
+export function useThemeColor(props, colorName) {
   const theme = useColorScheme() ?? 'light';
   const colorFromProps = props[theme];
 
