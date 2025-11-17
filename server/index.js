f7cb9f369860c648b7836a39c0e7a93e2d29e1e9
ikeshav26 <keshavgilhotra4@gmail.com>
2025-11-17 10:25:30 +0530

    Some stuff

--- server/index.js
+++ server/index.js
@@ -30,6 +30,6 @@ app.use('/api/user',userRoutes)
 app.use('/api/admin',adminRoutes)
 app.use('/api/notifications',notificationRoutes)
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`)
-})
+app.listen(process.env.PORT, "0.0.0.0", () => {
+  console.log(`Server is running on http://10.53.2.26:${process.env.PORT}`);
+});
